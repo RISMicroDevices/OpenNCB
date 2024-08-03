309727eeea4b00f26ce9e152ff2ae9231a7f0c26
Kumonda221 <kumonda@kucro3.org>
2024-08-03 20:23:07 +0800

    AXI: refined axi interfaces

--- src/main/scala/openncb/axi/AbstractAXI4Interface.scala
+++ src/main/scala/openncb/axi/AbstractAXI4Interface.scala
@@ -1,5 +1,6 @@
 package cn.rismd.openncb.axi
 
 import chisel3.Bundle
+import org.chipsalliance.cde.config.Parameters
 
-abstract class AbstractAXI4Interface extends Bundle
+abstract class AbstractAXI4Interface(implicit val p: Parameters) extends Bundle
