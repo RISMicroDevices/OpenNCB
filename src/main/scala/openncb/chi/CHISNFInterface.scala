fe52e67f6e5969910146c74087eec4ccdf4d72d5
Kumonda221 <kumonda@kucro3.org>
2024-08-05 14:59:03 +0800

    CHI: SN-F interface

---
+++ src/main/scala/openncb/chi/CHISNFInterface.scala
@@ -0,0 +1,35 @@
+package cn.rismd.openncb.chi
+
+import chisel3._
+import org.chipsalliance.cde.config.Parameters
+
+
+/*
+* CHI SN-F node interface. 
+*/
+class CHISNFInterface(implicit val p: Parameters) extends Bundle with WithCHIParameters {
+
+    // RXREQ
+    val rxreq               = CHIChannelRXREQ()
+
+    // TXRSP
+    val txrsp               = CHIChannelTXRSP()
+
+    // TXDAT
+    val txdat               = CHIChannelTXDAT()
+
+    // RXDAT
+    val rxdat               = CHIChannelRXDAT()
+
+    // RXLINKACTIVE
+    val rxlinkactivereq     = Input(Bool())
+    val rxlinkactiveack     = Output(Bool())
+
+    // TXLINKACTIVE
+    val txlinkactivereq     = Output(Bool())
+    val txlinkactiveack     = Input(Bool())
+}
+
+object CHISNFInterface {
+    def apply()(implicit p: Parameters) = new CHISNFInterface
+}
