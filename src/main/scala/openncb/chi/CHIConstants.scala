1801dcf40f3188f1951231d5867a1c7f40c81b38
Kumonda221 <kumonda@kucro3.org>
2024-08-05 14:58:44 +0800

    CHI: specification constants

---
+++ src/main/scala/openncb/chi/CHIConstants.scala
@@ -0,0 +1,10 @@
+package cn.rismd.openncb.chi
+
+object CHIConstants {
+
+    /*
+    * Maximum Link Credit count for CHI specification,
+    * which is always {@value 15}. 
+    */
+    val CHI_MAX_REASONABLE_LINK_CREDIT_COUNT    : Int   = 15
+}
