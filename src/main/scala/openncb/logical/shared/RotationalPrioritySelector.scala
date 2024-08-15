9cc896f4f19e016146709d11dbbb4a0150c745c2
Kumonda221 <kumonda@kucro3.org>
2024-08-15 17:36:03 +0800

    logical: shared: revised code style of RotationalPrioritySelector

--- src/main/scala/openncb/logical/shared/RotationalPrioritySelector.scala
+++ src/main/scala/openncb/logical/shared/RotationalPrioritySelector.scala
@@ -49,6 +49,16 @@ import chisel3.util.log2Up
 class RotationalPrioritySelector(val sourceCount: Int) extends Module {
 
     /*
+    * Bundle of leaf Node module port I/O. 
+    */
+    protected class LeafPort(val layer: Int) extends Bundle {
+        val valid       = Input(Bool())
+        val index       = Input(UInt((layer + 1).W))
+        val select      = Output(UInt((layer + 1).W))
+    }
+
+
+    /*
     * Selector Node at Upper Layer.
     * 
     * * Applies to nodes at height of 3 and higher (node height starts from 1).
@@ -60,39 +70,32 @@ class RotationalPrioritySelector(val sourceCount: Int) extends Module {
         // Module I/O
         val io = IO(new Bundle {
             //
-            val lower_l_in_valid    = Input(Bool())
-            val lower_l_in_index    = Input(UInt((layer + 1).W))
-            val lower_l_out_select  = Output(UInt((layer + 1).W))
-
-            val lower_r_in_valid    = Input(Bool())
-            val lower_r_in_index    = Input(UInt((layer + 1).W))
-            val lower_r_out_select  = Output(UInt((layer + 1).W))
+            val ll      = new LeafPort(layer)
+            val lr      = new LeafPort(layer)
 
             //
-            val upper_in_select     = Input(UInt((layer + 2).W))
-            val upper_out_valid     = Output(Bool())
-            val upper_out_index     = Output(UInt((layer + 2).W))
+            val up      = Flipped(new LeafPort(layer + 1))
         })
 
         // 
-        protected val select_down   = io.upper_in_select(layer, 0)
-        protected val select_flat   = io.upper_in_select(layer + 1)
+        protected val select_down   = io.up.select(layer, 0)
+        protected val select_flat   = io.up.select(layer + 1)
 
         //
-        protected val select_l_reverse  = !io.lower_l_in_valid || (io.lower_r_in_valid && (select_down > io.lower_l_in_index))
-        protected val select_r_reverse  = !io.lower_r_in_valid || (io.lower_l_in_valid && (select_down > io.lower_r_in_index))
+        protected val select_l_reverse  = !io.ll.valid || (io.lr.valid && (select_down > io.ll.index))
+        protected val select_r_reverse  = !io.lr.valid || (io.ll.valid && (select_down > io.lr.index))
 
         //
-        protected val selected_l_index  = Cat(1.U, io.lower_l_in_index)
-        protected val selected_r_index  = Cat(0.U, io.lower_r_in_index)
+        protected val selected_l_index  = Cat(1.U, io.ll.index)
+        protected val selected_r_index  = Cat(0.U, io.lr.index)
 
         // 
-        io.lower_l_out_select   := Mux( select_flat, select_down, 0.U)
-        io.lower_r_out_select   := Mux(!select_flat, select_down, 0.U)
+        io.ll.select    := Mux( select_flat, select_down, 0.U)
+        io.lr.select    := Mux(!select_flat, select_down, 0.U)
 
         //
-        io.upper_out_valid  := io.lower_l_in_valid | io.lower_r_in_valid
-        io.upper_out_index  := Mux(select_flat,
+        io.up.valid     := io.ll.valid | io.lr.valid
+        io.up.index     := Mux(select_flat,
             Mux(select_l_reverse, selected_r_index, selected_l_index),
             Mux(select_r_reverse, selected_l_index, selected_r_index))
     }
@@ -107,23 +110,21 @@ class RotationalPrioritySelector(val sourceCount: Int) extends Module {
         // Module I/O
         val io = IO(new Bundle {
             //
-            val lower_l_in_valid    = Input(Bool())
+            val ll_valid    = Input(Bool())
 
-            val lower_r_in_valid    = Input(Bool())
+            val lr_valid    = Input(Bool())
 
             //
-            val upper_in_select     = Input(Bool())
-            val upper_out_valid     = Output(Bool())
-            val upper_out_index     = Output(UInt(1.W))
+            val up          = Flipped(new LeafPort(0))
         })
 
         // selection logic
         protected val select = Wire(Bool())
-        select := (io.upper_in_select && io.lower_l_in_valid) || !io.lower_r_in_valid
+        select  := (io.up.select.asBool && io.ll_valid) || !io.lr_valid
 
         //
-        io.upper_out_valid  := io.lower_l_in_valid | io.lower_r_in_valid
-        io.upper_out_index  := Mux(select, 1.U, 0.U)
+        io.up.valid := io.ll_valid | io.lr_valid
+        io.up.index := Mux(select, 1.U, 0.U)
     }
 
     /* 
@@ -196,32 +197,24 @@ class RotationalPrioritySelector(val sourceCount: Int) extends Module {
         firstLayerNodes(i) = Module(new NodeFirstLayer)
     
     firstLayerNodes.zipWithIndex.foreach { case (node, i) => {
-        node.io.lower_r_in_valid := terminals((i << 1) + 0).io.upper_out_valid
-        node.io.lower_l_in_valid := terminals((i << 1) + 1).io.upper_out_valid
+        node.io.lr_valid := terminals((i << 1) + 0).io.upper_out_valid
+        node.io.ll_valid := terminals((i << 1) + 1).io.upper_out_valid
     }}
 
     //
     if ((sourceCountUp >> 2) != 0) {
 
-        val upperLayerNodes = new Array[Array[NodeUpperLayer]](sourceIndexWidth - 1)
+        val upperLayerNodes = new Array[Vector[NodeUpperLayer]](sourceIndexWidth - 1)
 
         // first upper layer
-        upperLayerNodes(0) = new Array[NodeUpperLayer](sourceCountUp >> 2)
+        upperLayerNodes(0) = Vector.tabulate(sourceCountUp >> 2) { (i) =>
 
-        for (i <- 0 until upperLayerNodes(0).length)
-            upperLayerNodes(0)(i) = Module(new NodeUpperLayer(0))
-        
-        upperLayerNodes(0).zipWithIndex.foreach { case (node, i) => {
+            val node    = Module(new NodeUpperLayer(0))
 
-            firstLayerNodes((i << 1) + 0).io.upper_in_select    := node.io.lower_r_out_select
-            firstLayerNodes((i << 1) + 1).io.upper_in_select    := node.io.lower_l_out_select
-
-            node.io.lower_r_in_index    := firstLayerNodes((i << 1) + 0).io.upper_out_index
-            node.io.lower_l_in_index    := firstLayerNodes((i << 1) + 1).io.upper_out_index
-
-            node.io.lower_r_in_valid    := firstLayerNodes((i << 1) + 0).io.upper_out_valid
-            node.io.lower_l_in_valid    := firstLayerNodes((i << 1) + 1).io.upper_out_valid
-        }}
+            node.io.lr <> firstLayerNodes((i << 1) + 0).io.up
+            node.io.ll <> firstLayerNodes((i << 1) + 1).io.up
+            node
+        }
 
         // upper upper layer
         if ((sourceCountUp >> 3) != 0) {
@@ -231,37 +224,29 @@ class RotationalPrioritySelector(val sourceCount: Int) extends Module {
 
             while (treeWidth > 0) {
 
-                upperLayerNodes(treeLayer) = new Array[NodeUpperLayer](treeWidth)
+                upperLayerNodes(treeLayer) = Vector.tabulate(treeWidth) { (i) =>
 
-                for (i <- 0 until upperLayerNodes(treeLayer).length) 
-                    upperLayerNodes(treeLayer)(i) = Module(new NodeUpperLayer(treeLayer))
-                
-                upperLayerNodes(treeLayer).zipWithIndex.foreach { case (node, i) => {
+                    val node    = Module(new NodeUpperLayer(treeLayer))
 
-                    upperLayerNodes(treeLayer - 1)((i << 1) + 0).io.upper_in_select := node.io.lower_r_out_select
-                    upperLayerNodes(treeLayer - 1)((i << 1) + 1).io.upper_in_select := node.io.lower_l_out_select
-
-                    node.io.lower_r_in_index    := upperLayerNodes(treeLayer - 1)((i << 1) + 0).io.upper_out_index
-                    node.io.lower_l_in_index    := upperLayerNodes(treeLayer - 1)((i << 1) + 1).io.upper_out_index
-
-                    node.io.lower_r_in_valid    := upperLayerNodes(treeLayer - 1)((i << 1) + 0).io.upper_out_valid
-                    node.io.lower_l_in_valid    := upperLayerNodes(treeLayer - 1)((i << 1) + 1).io.upper_out_valid
-                }}
+                    node.io.lr <> upperLayerNodes(treeLayer - 1)((i << 1) + 0).io.up
+                    node.io.ll <> upperLayerNodes(treeLayer - 1)((i << 1) + 1).io.up
+                    node
+                }
 
                 treeWidth >>= 1
                 treeLayer  += 1
             }
         }
 
-        upperLayerNodes.last(0).io.upper_in_select  := io.in_head
+        upperLayerNodes.last(0).io.up.select    := io.in_head
 
-        io.out_valid    := upperLayerNodes.last(0).io.upper_out_valid
-        io.out_index    := upperLayerNodes.last(0).io.upper_out_index
+        io.out_valid    := upperLayerNodes.last(0).io.up.valid
+        io.out_index    := upperLayerNodes.last(0).io.up.index
     } else {
 
-        firstLayerNodes(0).io.upper_in_select   := io.in_head(0)
+        firstLayerNodes(0).io.up.select := io.in_head
 
-        io.out_valid    := firstLayerNodes(0).io.upper_out_valid
-        io.out_index    := firstLayerNodes(0).io.upper_out_index
+        io.out_valid    := firstLayerNodes(0).io.up.valid
+        io.out_index    := firstLayerNodes(0).io.up.index
     }
 }
