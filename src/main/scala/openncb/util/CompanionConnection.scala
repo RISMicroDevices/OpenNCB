7fcb598b3bde0fd526723d909a1bdc48607e53bb
Kumonda221 <kumonda@kucro3.org>
2024-08-27 20:57:42 +0800

    utility: CompanionConnection annotation

---
+++ src/main/scala/openncb/util/CompanionConnection.scala
@@ -0,0 +1,11 @@
+package cn.rismd.openncb.debug
+
+import scala.annotation.StaticAnnotation
+
+
+/*
+* Base class for debug signals.
+* 
+* * NOTICE: Reserved for furture use, recommended to embed this in code. 
+*/
+final class CompanionConnection extends StaticAnnotation
