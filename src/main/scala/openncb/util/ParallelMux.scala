434abfaf5b55ffb1caa52599f9c0ee96a19708bf
Kumonda221 <kumonda@kucro3.org>
2024-08-27 20:57:23 +0800

    utility: ParallelMux

---
+++ src/main/scala/openncb/util/ParallelMux.scala
@@ -0,0 +1,58 @@
+package cn.rismd.openncb.util
+
+import chisel3._
+
+
+/* 
+* Hardware parallel reduction.
+*/
+object ParallelReduction {
+
+    /*
+    * @param x      Reduction subject.
+    * @param func   Reduction function.
+    */
+    def apply[T <: Data](x: Seq[T], func: (T, T) => T): T = {
+
+        require(x.nonEmpty)
+
+        x match {
+
+            case Seq(a)     => a
+            case Seq(a, b)  => func(a, b)
+            case _          => apply(Seq(
+                    apply(x.take(x.size / 2), func),
+                    apply(x.drop(x.size / 2), func)), 
+                func)
+        }
+    }
+}
+
+
+/*
+* Hardware parallel OR reduction.
+*/
+object ParallelOR {
+
+    /* 
+    * @param x      Reduction subject.
+    */
+    def apply[T <: Data](x: Seq[T]): T =
+        ParallelReduction(x, (a: T, b: T) => (a.asUInt | b.asUInt).asTypeOf(x.head))
+}
+
+
+/* 
+* Hardware parallel One-hot MUX.
+*/
+object ParallelMux {
+
+    /*
+    * @param in     Sequence of (Selection Enable, Data). 
+    */
+    def apply[T <: Data](in: Seq[(Bool, T)]): T =
+        ParallelOR(in.map({ case (en, x) => {
+            val xu = x.asUInt
+            (xu & VecInit.fill(xu.getWidth)(en).asUInt).asTypeOf(x)
+        } }))
+}
