a237efac5c08279a79c5b412c04bed172102bdb5
Kumonda221 <kumonda@kucro3.org>
2024-09-10 13:34:58 +0800

    util: added package utility functions for IrrevocableIO and DecoupledIO

--- src/main/scala/openncb/util/package.scala
+++ src/main/scala/openncb/util/package.scala
@@ -2,6 +2,10 @@ package cc.xiangshan.openncb
 
 import chisel3._
 import chisel3.experimental.VecLiterals._
+import chisel3.util.IrrevocableIO
+import chisel3.util.Irrevocable
+import chisel3.util.DecoupledIO
+import chisel3.util.Decoupled
 
 
 package object util {
@@ -36,5 +40,87 @@ package object util {
         */
         def extract(offset: Int, width: Int): UInt =
             data((offset + 1) * width - 1, offset * width)
+    }
+
+    implicit class irrevocableIOMapFunction[+T <: Data](gen: IrrevocableIO[T]) {
+
+        /** Applies the supplied functor to the bits of this interface, returning a new
+        * typed IrrevocableIO interface, with direciton of:
+        *   B = A.mapTo(B) : 
+        *      B.bits  := A.bits
+        *      B.valid := A.valid
+        *      A.ready := B.ready                
+        * 
+        * @param f The function to apply to this IrrevocableIO's 'bits' with return type B
+        * @return a new IrrevocableIO of type B
+        */
+        def mapTo[B <: Data](f: T => B): IrrevocableIO[B] = {
+            val _map_bits = f(gen.bits)
+            val _map = Wire(Irrevocable(chiselTypeOf(_map_bits)))
+            _map.bits   := _map_bits
+            _map.valid  := gen.valid
+            gen.ready   := _map.ready
+             _map
+        }
+
+        /** Applies the supplied functor to the bits of this interface, returning a new
+        * typed IrrevocableIO interface, with direciton of:
+        *   B = A.mapFrom(B) : 
+        *      A.bits  := B.bits
+        *      A.valid := B.valid
+        *      B.ready := A.ready                
+        * 
+        * @param f The function to apply to this IrrevocableIO's 'bits' with return type B
+        * @return a new IrrevocableIO of type B
+        */
+        def mapFrom[B <: Data](f: T => B): IrrevocableIO[B] = {
+            val _map_bits = f(gen.bits)
+            val _map = Wire(Irrevocable(chiselTypeOf(_map_bits)))
+            _map.bits   := _map_bits
+            gen.valid   := _map.valid
+            _map.ready  := gen.ready
+             _map
+        }
+    }
+
+    implicit class decoupledIOMapFunction[+T <: Data](gen: DecoupledIO[T]) {
+
+        /** Applies the supplied functor to the bits of this interface, returning a new
+        * typed DecoupledIO interface, with direciton of:
+        *   A.mapTo(B) : 
+        *      B.bits  := A.bits
+        *      B.valid := A.valid
+        *      A.ready := B.ready                
+        * 
+        * @param f The function to apply to this DecoupledIO's 'bits' with return type B
+        * @return a new DecoupledIO of type B
+        */
+        def mapTo[B <: Data](f: T => B): DecoupledIO[B] = {
+            val _map_bits = f(gen.bits)
+            val _map = Wire(Decoupled(chiselTypeOf(_map_bits)))
+            _map.bits   := _map_bits
+            _map.valid  := gen.valid
+            gen.ready   := _map.ready
+             _map
+        }
+
+        /** Applies the supplied functor to the bits of this interface, returning a new
+        * typed DecoupledIO interface, with direciton of:
+        *   A.mapFrom(B) : 
+        *      A.bits  := B.bits
+        *      A.valid := B.valid
+        *      B.ready := A.ready                
+        * 
+        * @param f The function to apply to this DecoupledIO's 'bits' with return type B
+        * @return a new DecoupledIO of type B
+        */
+        def mapFrom[B <: Data](f: T => B): DecoupledIO[B] = {
+            val _map_bits = f(gen.bits)
+            val _map = Wire(Decoupled(chiselTypeOf(_map_bits)))
+            _map.bits   := _map_bits
+            gen.valid   := _map.valid
+            _map.ready  := gen.ready
+             _map
+        }
     }
 }
